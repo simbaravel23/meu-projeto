59f858c0d5918d711fc675ea265aa9b628cd4cb9
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2024-08-16 09:13:24 -0300

    funçoes js no react

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@
 import './App.css';
 
 function App() {
+
+ const name = 'Ravel'
+ const newName = name.toLocaleUpperCase()
+
+ function sum (a, b){
+    return a + b
+ }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Olá React</h1>
         <p>Meu primeiro app</p>
+        <p>Olá, {newName}</p>
+        <p>Soma: {sum(1+2)}</p>
       </header>
     </div>
   );
